e65698f67983ef6681f1ed611ce744131910e854
Jean Costello <costellojeanm@gmail.com>
2021-06-07 14:50:15 -0700

    use datatables for exposure

--- exposure_cml.R
+++ exposure_cml.R
@@ -1,4 +1,5 @@
 # this R script exists to calculate exposure for every pregnancy in our dataset.
+# it's called from exposure_cml.sh
 # exposure is cumulative
 # for now, filter out pregnancies which last into 2021 since we don't have that data yet
 
@@ -8,15 +9,11 @@
 # do a left join (or datatable equivalent)
 # summarize by summing over each pregnancy
 
-# figure out how to test this on a subset first; it's going to be horrible
 library(lubridate)
 library(dplyr)
 library(data.table)
-print(Sys.time())
 files = list.files("/c4/home/jcostello/covid/output/values_extracted1/", full.names = TRUE)
-#air_vals <- lapply(files[1:3], fread)  # this works, but each file is a separate element of dt
-#air_vals <- rbind(lapply(files[1:5], fread, header=TRUE))
-air_vals <- do.call(rbind, lapply(files[1:3], fread, header=TRUE))
+air_vals <- do.call(rbind, lapply(files, fread, header=TRUE))
 print(paste('air data read', Sys.time()))
 setnames(air_vals, new = c('longitude', 'latitude', 'layer', 'date'))
 
@@ -25,10 +22,9 @@ setnames(air_vals, new = c('longitude', 'latitude', 'layer', 'date'))
 # read in the births data, simplify it to the columns we need (ID, longitude, latitude, start, end)
 # spread/pivot the births data over the date range (by day)
 births <- read.csv('/c4/home/jcostello/covid/data/births_exposure_calc.csv')
-births_mini <- sample_n(births, 500)
-print(births_mini %>% filter(is.na(start_date)|is.na(baby_DOB)))
-print(nrow(births_mini))
-births_exp <- births_mini %>% 
+#births_mini <- sample_n(births, 500)
+print(paste('births data read', Sys.time()))
+births_exp <- births %>% 
    rowwise() %>%
    do(data.frame(VS_unique = .$VS_unique, longitude = .$longitude, latitude = .$latitude, start_date = .$start_date, baby_DOB =.$baby_DOB, day_date = seq(ymd(.$start_date), ymd(.$baby_DOB), by = 'day')))
 print(paste('births data read and spread', Sys.time()))
